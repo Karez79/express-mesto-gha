626bd4334c8e8e2655b0062df35bd015f011aad0
Karez79 <118558248+Karez79@users.noreply.github.com>
2023-08-05 21:33:59 +0300

    Add celebrate and .env

--- app.js
+++ app.js
@@ -4,25 +4,41 @@ const helmet = require('helmet');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const { celebrate, Joi, errors } = require('celebrate');
 const usersRouter = require('./routes/user');
 const cardsRouter = require('./routes/card');
 const errorMiddleware = require('./middlewares/error');
 const authMiddleware = require('./middlewares/auth');
 const ApiError = require('./exceptions/api-error');
 const userController = require('./controllers/user');
+const avatarUrlRegexp = require('./services/avatarUrlRegexp');
 
 const app = express();
 
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.post('/signin', userController.login);
-app.post('/signup', userController.createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), userController.login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(avatarUrlRegexp),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), userController.createUser);
 app.use('/users', authMiddleware, usersRouter);
 app.use('/cards', authMiddleware, cardsRouter);
 app.all('*', (req, res, next) => {
   next(ApiError.NotFound('Неверный URL'));
 });
+app.use(errors());
 app.use(errorMiddleware);
 
 const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
